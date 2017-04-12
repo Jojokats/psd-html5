b1a7c5c98f994602a23527eb26837c82e1503fb8
Joanna <morningstarjk@hotmail.com>
2017-04-12 12:49:21 -0400

    modified app.css index.htmland main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 $(document).ready (function() {
 	
-	$('.js-wp-2').waypoint(function(direction) {
+	new WOW().init(); // we are initializing the WOW plugin
+	
+	$('.js-wp-2').waypoint(function() {
 		
 		$('.js-wp-2').addClass('animated slideInUp');
 		
 	}, {
 		offset: '70%'
 	});
+	
+	$('.js-wp-3').waypoint(function(direction) {
+
+		$('.js-wp-3').addClass('animated fadeIn');
+
+	}, {
+		offset: '70%'
+	});
+	
+	$(".iphone-btn").delay(2300).animate( {bottom: "+=-3"}, 300);
+	$(".iphone-btn").delay(300).animate( {top: "+=-3"}, 100);
 	
 });
